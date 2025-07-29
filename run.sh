fa5219f4077e34e2eec4f3d36b4ba005ccb5794c
emory <emorywrussell@gmail.com>
2025-07-29 14:24:37 +0000

    12g

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 tmux new-session -d -s minecraft-server
-tmux send-keys -t minecraft-server "java -Xmx16G -Xms16G -jar fabric-server nogui" C-m
+tmux send-keys -t minecraft-server "java -Xmx12G -Xms12G -jar fabric-server nogui" C-m
